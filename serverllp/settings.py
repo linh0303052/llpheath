7159b85cbac941d1cd8e34bd288a52f6fdb68b2c
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-29 22:31:56 +0700

    Add files via upload

--- serverllp/settings.py
+++ serverllp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@^ha37^cbprde7svkb6ivs-gnz741q+yt@*tj-u8q1vzzb(yqq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['llpheath.herokuapp.com']
+ALLOWED_HOSTS = ['llpheath.herokuapp.com','localhost']
 
 
 # Application definition
@@ -122,16 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+# MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'ltt.lop9a1.lhlinh@gmail.com'
