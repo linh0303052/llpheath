c7dd1af20a888120434bc96f430109e1c7f87b62
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-29 22:36:53 +0700

    Add files via upload

--- serverllp/settings.py
+++ serverllp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@^ha37^cbprde7svkb6ivs-gnz741q+yt@*tj-u8q1vzzb(yqq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['llpheath.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['llpheath.herokuapp.com','127.0.0.1:8000', '127.0.0.1']
 
 
 # Application definition
