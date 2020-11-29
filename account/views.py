dbef0e058866c17f66b85c7f50a8ad9a069dfea4
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-30 00:03:54 +0700

    Add files via upload

--- account/views.py
+++ account/views.py
@@ -7,13 +7,9 @@ from django.core.mail import send_mail
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login as user_login
 from django.contrib.auth.forms import AuthenticationForm
+from django.utils.dateparse import parse_date
 
 import json
-
-@csrf_exempt
-def login(request):
-    print(request.content_type)
-    return HttpResponse()
 
 # Create your views here.
 @csrf_exempt
@@ -30,13 +26,13 @@ def register(request):
     if (len(user) > 0):
         data = {'success': False,
                 'message': 'Username existed!'}
-        return HttpResponse(json.dumps(data), content_type='application/json')
+        return HttpResponse(data)
 
     user = Account.objects.filter(email=email)
     if (len(user) > 0):
         data = {'success': False,
-                'message': 'Email existed!'}
-        return HttpResponse(json.dumps(data), content_type='application/json')
+                'message': 'Username existed!'}
+        return HttpResponse(data)
 
     password = request.POST['password']
     if (hasattr(request.POST,'first_name')):
@@ -47,7 +43,8 @@ def register(request):
         lastName = request.POST['last_name']
     else:
         lastName = 'Name'
-    dob = request.POST['dob']
+    dob = parse_date(request.POST['dob'])
+    print(dob.)
     if (hasattr(request.POST,'weight')):
         weight = request.POST['weight']
     else:
@@ -67,7 +64,7 @@ def register(request):
         data = {'success': True}
     else:
         data = {'success': False}
-    return HttpResponse(json.dumps(data), content_type='application/json')
+    return HttpResponse(data)
 
 @csrf_exempt
 def auth(request):
@@ -84,25 +81,57 @@ def auth(request):
 
 @csrf_exempt
 def forgot_password(request):
+    data = {'success': False}
     chars = 'abcdefghiklmnopqrstuvwxyz1234567890ABCDEFGHIKLMNOPQRSTUVWXYZ'
     password = ''
     for i in range(0, 8):
         password += random.choice(chars)
     username = request.POST['username']
-    user = Account.objects.get(username=username)
+
+    user = Account.objects.filter(username=username)
+    if (len(user) == 0):
+        data.message = 'username does not exist'
+        return HttpResponse(json.dumps(data), content_type='application/json')
+    
+    last_name = user.last_name
     email = user.email
     user.set_password(password)
     send_mail(
         subject='[LLP Health] Reset password',
-        message='Hello Linh Linh',
-        recipient_list=['ltt.lop9a1.lhlinh@gmail.com'],
+        message='''Dear {},
+                    Your password has been changed to:{}
+                    Log in with this new password and then change to another.
+                    Best regards.'''.format(last_name, password)
+        recipient_list=[email],
         from_email='ltt.lop9a1.lhlinh@gmail.com'
     )
+    data.success = True
+    return HttpResponse(json.dumps(data), content_type='application/json')
 
 
 @csrf_exempt
 def change_password(request):
+    data = {'success': False}
     username = request.POST['username']
     old_password = request.POST['old_pasword']
     new_password = request.POST['new_password']
     user = Account.objects.get(username=username)
+    if (user.check_password(old_password)):
+        user.set_password(new_password)
+        data.success = True
+    else:
+        data.message = 'wrong password'
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+
+@csrf_exempt
+def getUser(request):
+    data = {'success': False}
+    username = request.POST['username']
+    user = Account.objects.get(username=username)
+    data.dob = user.dob
+    data.first_name = user.first_name
+    data.last_name = user.last_name
+    data.email = user.email
+    data.success = True
+    return HttpResponse(json.dumps(data), content_type='application/json')
