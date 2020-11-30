a455d60f40b860bd8a1f7c3b8dffce2163a60854
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-30 15:34:31 +0700

    Add files via upload

--- exercise/views.py
+++ exercise/views.py
@@ -8,16 +8,13 @@ from django.http import HttpRequest, HttpResponse
 
 def get_exercise(request, username):
     history = JoinExercise.objects.filter(completed=False, user__username = username)
-    # all_exercise = Exercise.objects.all()
-    history_id = [i.exercise.id for i in history]
     new_exercises = Exercise.objects.all()
-    for i in new_exercises:
-        if i.id in history_id:
-            new_exercises[i].delete()
+
     data={'success':False}
     data['success']=True
     data['history'] = [i.toObject() for i in history]
     data['new_exercises'] = [i.toObject() for i in new_exercises]
+    
     #data['all'] = [i.toObject() for i in all_exercise]
     #data['new_exercises'] = new_exercises
     return HttpResponse(json.dumps(data), content_type='application/json')
