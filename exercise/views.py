d26fdfe701d6e9af528f2b12d923252cb5dbbb65
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-30 17:38:56 +0700

    Add files via upload

--- exercise/views.py
+++ exercise/views.py
@@ -4,17 +4,12 @@ import json
 from django.core import serializers
 from django.http import HttpRequest, HttpResponse
 
-# Create your views here
-def check_id(array_id, id):
-    for i in array_id:
-        if i == id:
-            return True
-    return False
+# Create your views her
 
 def get_exercise(request, username):
     history = JoinExercise.objects.filter(completed=False)
     history_id = [i.exercise.id for i in history]
-    new_exercises = Exercise.objects.exclude(check_id(history_id, id))
+    new_exercises = Exercise.objects.exclude(id in history_id)
 
     data={'success':False}
     data['success']=True
