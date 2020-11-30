a5f1ccbd2ff2183e47ac4e39eca4c3cb5570a4dd
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-30 14:38:59 +0700

    Add files via upload

--- exercise/views.py
+++ exercise/views.py
@@ -13,6 +13,6 @@ def get_exercise(request, username):
     data={'success':False}
     data['success']=True
     # data['history'] = history
-    data['all'] = all_exercise#[i.toObject() for i in all_exercise]
+    data['all'] = [i.toObject() for i in all_exercise]
     #data['new_exercises'] = new_exercises
     return HttpResponse(json.dumps(data), content_type='application/json')
