aa2a3b6c5840d2dd949f2b8d57310e313ef0acf4
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-30 13:27:51 +0700

    Add files via upload

--- exercise/views.py
+++ exercise/views.py
@@ -7,6 +7,7 @@ def get_exercise(request, username):
     #history = Exercise.objects.filter(joinexercise__completed=False, joinexercise__user__username = username)
     all_exercise = Exercise.objects.all()
     # new_exercises = Exercise.objects.filter(self not in history)
+    data={'success':False}
     data['success']=True
     # data['history'] = history
     data['all'] = all_exercise
