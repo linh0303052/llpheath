10de222fffec64d07d0fb13d97849a04715db4a4
linh0303052 <52354736+linh0303052@users.noreply.github.com>
2020-11-30 14:55:26 +0700

    Add files via upload

--- exercise/views.py
+++ exercise/views.py
@@ -7,12 +7,12 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def get_exercise(request, username):
-    #history = Exercise.objects.filter(joinexercise__completed=False, joinexercise__user__username = username)
-    all_exercise = Exercise.objects.all()
+    history = Exercise.objects.filter(joinexercise__completed=False, joinexercise__user__username = username)
+    # all_exercise = Exercise.objects.all()
     # new_exercises = Exercise.objects.filter(self not in history)
     data={'success':False}
     data['success']=True
-    # data['history'] = history
-    data['all'] = [i.toObject() for i in all_exercise]
+    data['history'] = [i.toObject() for i in history]
+    #data['all'] = [i.toObject() for i in all_exercise]
     #data['new_exercises'] = new_exercises
     return HttpResponse(json.dumps(data), content_type='application/json')
